4319db406e0b745eb5d17721e60cda332be71a97
ajmalmuhammed <muhammedajmal54@gmail.com>
2021-02-09 13:06:31 +0530

    Styling done

--- app.js
+++ app.js
@@ -9,27 +9,43 @@ LibraryController.$inject=['$scope'];
 
 function LibraryController($scope){
     $scope.listOfBooks=[];
+    $scope.book="";
+    $scope.messageStyle = {display : 'none'};
 
     $scope.enterValue = function(){
 
-        if($scope.book ==0){
+
+        if($scope.book==""){
             console.log($scope.book);
-            $scope.message = "Enter some value";
+            $scope.message = "Enter some value first";
+            $scope.isSuccess='false';
+            setMessageColor('red');
         }
         else if($scope.listOfBooks.indexOf($scope.book) != -1){
             console.log("Duplicate value");
-            $scope.message = "Duplicate Value found";
+            $scope.message = "Book already present";
+            setMessageColor('red');
+            $scope.book="";
         }
         else {
             $scope.listOfBooks.push($scope.book);
             console.log($scope.listOfBooks);
+            $scope.message = "Book added to list";
+            $scope.book="";
+            setMessageColor('green')
         }
             
     }
     $scope.deleteValue = function(){
         $scope.listOfBooks.splice($scope.index,1);
+        setMessageColor('red')
+        $scope.message="Book has been removed succesfully"
         
     }
+    function setMessageColor(color) {
+        $scope.messageStyle = {color: color};
+        $scope.textBoxStyle = {'border-color': color};
+      }
 
 }
 })();
